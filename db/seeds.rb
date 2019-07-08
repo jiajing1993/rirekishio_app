02530b47dd70337b00399ef505086fafbfb389a5
laserylee <laserylee2013@gmail.com>
2019-07-08 16:13:39 +0800

    Seeded admin and 10 fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Admin
+User.create!(email: "admin@admin.admin",
+             password: "1;r2;r3;r777",
+             password_confirmation: "1;r2;r3;r777",
+             admin: true,
+             confirmed_at: Time.zone.now)
+
+# First Ten Fake User
+10.times do |n|
+  email = "cus#{n+1}@test.org"
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               confirmed_at: Time.zone.now)
+end
