8cd5a711f7c1583fa69afcf74307deffc2e80e9c
laserylee <laserylee2013@gmail.com>
2019-07-08 15:23:22 +0800

    style: generate devise users controller and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "main_pages#home"
+
+  devise_for :users,
+    controllers: {
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations',
+      confirmations: 'users/confirmations'
+  }, path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'signup'
+  }
 end
