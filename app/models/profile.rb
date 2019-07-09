004a34ff4a074943ccbda9ccf2c69385e99755d4
laserylee <laserylee2013@gmail.com>
2019-07-09 14:03:43 +0800

    feat: validate profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,9 +7,10 @@
 #  email        :string
 #  phone_number :string
 #  current_role :string
-#  carrier_goal :string
+#  career_goal  :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
+#  user_id      :integer
 #
 
 class Profile < ApplicationRecord
@@ -19,4 +20,27 @@ class Profile < ApplicationRecord
   has_many :work_experiences
   has_many :education_backgrounds
   has_many :project_experiences
+
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 50 }
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: 255 },
+    format: { with: /\A[\w\d\-.]+@([a-z\d\-]+\.)+[a-z]+\z/i }
+  validates :phone_number,
+    presence: true,
+    format: { with: /\A[\d-]+[\s\d]+[\d]\z/ }
+    length: { maximum: 20 } # => currently no restrictions for phone number format
+  validates :current_role,
+    presence: true,
+    length: { maximum: 50 }
+  validates :career_goal,
+    presence: true,
+    length: { maximum: 255 }
+  validates :user_id,
+    presence: true,
+    uniqueness: true # => No user can have more than 1 profile
 end
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -7,15 +7,37 @@
 #  email        :string
 #  phone_number :string
 #  current_role :string
-#  carrier_goal :string
+#  career_goal  :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
+#  user_id      :integer
 #
 
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    legal_user = users(:one)
+
+    name_normal = "Lasery Lee"
+    name_long = "a"*51
+    
+    email_normal = "admin@admin.admin"
+    email_incorrect = "admin.admin"
+    email_long = "a"*256
+    email_upcase = "ADMIN@adMIN.admin"
+
+    phone_number_normal = "012-4319876"
+    phone_number_long = "234-451451-451-5135-15"
+    phone_number_space = "012-431 5378"
+    phone_number_space_end = "012-431 5434  "
+    
+    current_role_normal = "Chef"
+    current_role_long = "a"*51
+
+    career_goal_normal = "To be an inspiring chef."
+    career_goal_long = "a"*256
+  end
+
 end
