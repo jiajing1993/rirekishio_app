f2a236ea6d1d231ad2da6cc0e5617ae3cac624dd
laserylee <laserylee2013@gmail.com>
2019-07-09 14:07:14 +0800

    feat: set user admin boolean to default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_144125) do
+ActiveRecord::Schema.define(version: 2019_07_09_060543) do
 
   create_table "education_backgrounds", force: :cascade do |t|
     t.string "university"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_144125) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
