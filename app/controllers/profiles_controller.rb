c1f306c7e23b00bba6cc44ddb6ceea3beef4c312
laserylee <laserylee2013@gmail.com>
2019-07-11 22:55:08 +0800

    feat: enable create profile from params

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,7 @@ class ProfilesController < ApplicationController
   end
 
   def create
+    @profile = Profile.new(profile_params)
     byebug
   end
 
@@ -14,5 +15,32 @@ class ProfilesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def profile_params
+    profile = params.require(:profile).permit!.to_h
+    profile[:skills_attributes] = skills_array
+    profile[:work_experiences_attributes] = work_experience_array
+    profile[:education_backgrounds_attributes] = education_background_array
+    profile[:project_experiences_attributes] = project_experience_array
+    return profile
+  end
+
+  def skills_array
+    return params.require(:skills).permit!.to_h.values
+  end
+
+  def work_experience_array
+    return params.require(:work_experience).permit!.to_h.values
+  end
+
+  def education_background_array
+    return params.require(:education_background).permit!.to_h.values
+  end
+
+  def project_experience_array
+    return params.require(:project_experience).permit!.to_h.values
   end
 end
