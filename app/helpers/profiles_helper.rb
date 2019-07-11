a013e257ad4ea43129dffbdeb068d74f6b7e02f8
laserylee <laserylee2013@gmail.com>
2019-07-11 18:46:15 +0800

    style: move first_input and first_inputs (unused methods) under private in profile helper

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,4 +1,22 @@
 module ProfilesHelper
+  def load_skills
+    return File.read('app/assets/javascripts/skills.coffee').gsub!("index", "0").slice(11..-3)
+  end
+
+  def load_work_experience
+    return File.read('app/assets/javascripts/work_experience.coffee').gsub!("index", "0").slice(20..-3)
+  end
+
+  def load_education_background
+    return File.read('app/assets/javascripts/education_background.coffee').gsub!("index", "0").slice(25..-3)
+  end
+
+  def load_project_experience
+    return File.read('app/assets/javascripts/project_experience.coffee').gsub!("index", "0").slice(23..-3)
+  end
+
+  private
+
   # Generate first line of html text input
   # Both parent and child are string, function will generate a string
   def first_input(parent, child)
@@ -37,22 +55,5 @@ module ProfilesHelper
     result += '<button type="button" onclick="remove_field(' + @k + ',0)" >&#10006</button>'
 
     return result += '<br><br>'
-
-  end
-
-  def load_skills
-    return File.read('app/assets/javascripts/skills.coffee').gsub!("index", "0").slice(11..-3)
-  end
-
-  def load_work_experience
-    return File.read('app/assets/javascripts/work_experience.coffee').gsub!("index", "0").slice(20..-3)
-  end
-
-  def load_education_background
-    return File.read('app/assets/javascripts/education_background.coffee').gsub!("index", "0").slice(25..-3)
-  end
-
-  def load_project_experience
-    return File.read('app/assets/javascripts/project_experience.coffee').gsub!("index", "0").slice(23..-3)
   end
 end
