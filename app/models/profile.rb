5cfd072b8f8cb0e8ab374d0c59a4af65c272f89b
laserylee <laserylee2013@gmail.com>
2019-07-13 12:24:04 +0800

    feat: validates picture, validates profile even if not logged in yet, remove final line of cookie every time remove a line to prevent ghost lines that are deleted to return back every time page refresh

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -45,7 +45,9 @@ class Profile < ApplicationRecord
   validates :career_goal,
     presence: true,
     length: { maximum: 255 }
- # validates :user_id,
- #   presence: true,
- #   uniqueness: true # => No user can have more than 1 profile
+  validates :user_id,
+    presence: true,
+    uniqueness: true # => No user can have more than 1 profile
+  validates :picture,
+    presence: true
 end
