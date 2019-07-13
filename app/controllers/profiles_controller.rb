9235627d4d5d75103f6d1a4129fafc3410c8b371
laserylee <laserylee2013@gmail.com>
2019-07-13 11:41:19 +0800

    feat: enforce strong parameters

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
 
   def create
     @profile = Profile.new(profile_params)
-    if @profile.save
+    if @profile.valid?
       byebug
     else
       render :new
@@ -24,7 +24,7 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    profile = params.require(:profile).permit!.to_h
+    profile = params.require(:profile).permit(:name, :email, :phone_number, :current_role, :career_goal)
     profile[:skills_attributes] = skills_array
     profile[:work_experiences_attributes] = work_experience_array
     profile[:education_backgrounds_attributes] = education_background_array
@@ -33,18 +33,18 @@ class ProfilesController < ApplicationController
   end
 
   def skills_array
-    return params.require(:skills).permit!.to_h.values
+    return params.require(:skills).values.map { |x| x.slice(:proficiency, :skills, :description) }
   end
 
   def work_experience_array
-    return params.require(:work_experience).permit!.to_h.values
+    return params.require(:work_experience).values.map { |x| x.slice(:company, :role, :start_date, :end_date, :city, :description) }
   end
 
   def education_background_array
-    return params.require(:education_background).permit!.to_h.values
+    return params.require(:education_background).values.map { |x| x.slice(:graduation_date, :university, :major) }
   end
 
   def project_experience_array
-    return params.require(:project_experience).permit!.to_h.values
+    return params.require(:project_experience).values.map { |x| x.slice(:name, :link, :description) }
   end
 end
