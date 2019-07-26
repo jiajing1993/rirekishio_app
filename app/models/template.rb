33245d7e08832fc5ab2cee2685622cc39360f062
Laserylee <laserylee2013@gmail.com>
2019-07-26 19:56:01 +0800

    feat: restrict format of template name input and data

--- app/models/template.rb
+++ app/models/template.rb
@@ -10,4 +10,17 @@
 #
 
 class Template < ApplicationRecord
+  before_save :underscore_name
+
+  validates :name,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    format: { with: /\A(([a-z]+(_[a-z]+)*)|([a-z]+(\s[a-z]+)*))\z/i }
+
+  private
+
+  def underscore_name
+    self.name = name.downcase
+    self.name = name.gsub(/\s/, '_')
+  end
 end
