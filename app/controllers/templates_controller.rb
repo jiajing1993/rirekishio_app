86fc03f8ef908dc7c895f852c4298e2b2f7cba6b
Laserylee <laserylee2013@gmail.com>
2019-07-26 18:52:55 +0800

    feat: create template index, new, create and show actions

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,2 +1,41 @@
 class TemplatesController < ApplicationController
+  def index
+    @templates = Template.all
+  end
+
+  def show
+    @template = Template.find(params[:id])
+  end
+
+  def new
+    @template = Template.new
+  end
+
+  def create
+    @template = Template.new(template_params)
+    if @template.save
+      redirect_to edit_template_path(@template)
+      return
+    else
+      flash[:alert] = @template.errors.full_messages
+      redirect_to templates_path
+    end
+  end
+
+  def edit
+    @template = Template.find(params[:id])
+  end
+
+  def update
+    @template = Template.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  private
+
+  def template_params
+    params.require(:template).permit(:name, :content)
+  end
 end
